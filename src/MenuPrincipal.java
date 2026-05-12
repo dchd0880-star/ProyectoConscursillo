ee2d9d46aed7430446ca23bdaaad120ae53fbe51
daniel <dchd0880@gmail.com>
2026-05-12 09:32:13 +0200

    Tercer Commit

--- src/MenuPrincipal.java
+++ src/MenuPrincipal.java
@@ -20,9 +20,7 @@ public class MenuPrincipal extends JFrame {
 	private JPanel contentPane;
 	private JTextField txtNombre;
 
-	/**
-	 * Launch the application.
-	 */
+	
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
@@ -36,10 +34,9 @@ public class MenuPrincipal extends JFrame {
 		});
 	}
 
-	/**
-	 * Create the frame.
-	 */
+
 	public MenuPrincipal() {
+		setTitle("El Concursillo");
 		setIconImage(Toolkit.getDefaultToolkit().getImage(MenuPrincipal.class.getResource("/imagenes/IconoPrincipal.png")));
 		setResizable(false);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -52,18 +49,20 @@ public class MenuPrincipal extends JFrame {
 		contentPane.setLayout(null);
 		
 		txtNombre = new JTextField();
-		txtNombre.setBounds(342, 487, 202, 33);
+		txtNombre.setBounds(690, 177, 202, 33);
 		contentPane.add(txtNombre);
 		txtNombre.setColumns(10);
 		
-		JLabel lblNombre = new JLabel("Nombre");
+		JLabel lblNombre = new JLabel("Nombre Jugador");
 		lblNombre.setForeground(new Color(255, 255, 255));
 		lblNombre.setHorizontalAlignment(SwingConstants.CENTER);
-		lblNombre.setFont(new Font("Sylfaen", Font.PLAIN, 17));
-		lblNombre.setBounds(331, 455, 218, 33);
+		lblNombre.setFont(new Font("Sylfaen", Font.BOLD, 17));
+		lblNombre.setBounds(680, 148, 218, 33);
 		contentPane.add(lblNombre);
 		
-		JButton btnNuevaPartida = new JButton("Empezar Paritda");
+		JButton btnNuevaPartida = new JButton("Empezar Partida");
+		btnNuevaPartida.setSelectedIcon(null);
+		btnNuevaPartida.setIcon(new ImageIcon(MenuPrincipal.class.getResource("/ImagenesBotones/BotonEmpezar2.png")));
 		btnNuevaPartida.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 			    String nombreUsuario = txtNombre.getText();
@@ -84,14 +83,17 @@ public class MenuPrincipal extends JFrame {
 		        }
 		    }
 		});
-		btnNuevaPartida.setBounds(273, 559, 352, 46);
+		btnNuevaPartida.setBounds(52, 428, 495, 79);
 		contentPane.add(btnNuevaPartida);
 		
-		JButton btnRanking = new JButton("Ver Ranking");
-		btnRanking.setBounds(647, 559, 226, 46);
+		JButton btnRanking = new JButton("");
+		btnRanking.setIcon(new ImageIcon(MenuPrincipal.class.getResource("/ImagenesBotones/BotonRanking2.png")));
+		btnRanking.setBounds(132, 537, 324, 63);
 		contentPane.add(btnRanking);
 		
-		JButton btnInfo = new JButton("Info Juego");
+		JButton btnInfo = new JButton("");
+		btnInfo.setBackground(new Color(255, 255, 255));
+		btnInfo.setIcon(new ImageIcon(MenuPrincipal.class.getResource("/ImagenesBotones/BotonInfo.png")));
 		btnInfo.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 			    String reglas = "REGLAS DE EL CONCURSILLO:\n\n" +
@@ -103,13 +105,18 @@ public class MenuPrincipal extends JFrame {
 			    javax.swing.JOptionPane.showMessageDialog(null, reglas, "Información del Juego", javax.swing.JOptionPane.INFORMATION_MESSAGE);
 			}
 		});
-		btnInfo.setBounds(22, 559, 226, 46);
+		btnInfo.setBounds(680, 11, 218, 126);
 		contentPane.add(btnInfo);
 		
 		JLabel lblNewLabel_1 = new JLabel("");
 		lblNewLabel_1.setIcon(new ImageIcon(MenuPrincipal.class.getResource("/imagenes/LetrasApp.png")));
-		lblNewLabel_1.setBounds(173, 66, 518, 352);
+		lblNewLabel_1.setBounds(29, 35, 518, 352);
 		contentPane.add(lblNewLabel_1);
+		
+		JLabel lblNewLabel = new JLabel("");
+		lblNewLabel.setIcon(new ImageIcon(MenuPrincipal.class.getResource("/imagenes/Illojuan .png")));
+		lblNewLabel.setBounds(574, 247, 602, 463);
+		contentPane.add(lblNewLabel);
 
 	}
 }
